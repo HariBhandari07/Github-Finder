c5faf5d822b62dc26f9fd22fbd71be1203b679fd
HariBhandari07 <bhandarihari07@gmail.com>
2020-08-31 17:44:49 +0545

    Create reducer and actions

--- src/App.js
+++ src/App.js
@@ -17,14 +17,7 @@ const App = () => {
     const [loading, setLoading] = useState(false)
     const [alert, setAlert] = useState(null)
 
-    // search Github users
-    const searchUsers = async text => {
-        setLoading(true)
-        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-        setUsers(res.data.items)
-        setLoading(false)
-    }
+
 
     // Get a single github user
     const getUser = async username => {
@@ -66,8 +59,7 @@ const App = () => {
                         <Switch>
                             <Route exact path='/' render={props => (
                                 <Fragment>
-                                    <Search searchUsers={searchUsers}
-                                            clearUsers={clearUsers}
+                                    <Search
                                             showClear={users.length > 0 ? true : false}
                                             setAlert={showAlert} />
                                     <Users loading={loading} users={users} />
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import PropTypes from 'prop-types'
+import GithubContext from '../../context/github/githubContext'
 
-const Search = ({ searchUsers, setAlert, showClear, clearUsers }) => {
+const Search = ({ setAlert, showClear, clearUsers }) => {
+    const githubContext = useContext(GithubContext)
     const [text, setText] = useState('')
 
     const onChange = (e) => setText(e.target.value)
@@ -11,7 +13,7 @@ const Search = ({ searchUsers, setAlert, showClear, clearUsers }) => {
         if (text === '') {
             setAlert('Please enter something', 'light')
         } else {
-            searchUsers(text)
+            githubContext.searchUsers(text)
             setText('')
         }
     }
@@ -34,7 +36,6 @@ const Search = ({ searchUsers, setAlert, showClear, clearUsers }) => {
 }
 
 Search.propTypes = {
-    searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
     setAlert: PropTypes.func.isRequired
